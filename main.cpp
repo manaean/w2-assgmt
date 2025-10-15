1ecbfcb419fd3f7956e13dd1beb6ee42842be8f0
manaean <eanmana678@gmail.com>
2025-10-15 21:13:28 +0700

    Add files via upload

---
+++ main.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+#include "linkedlist.hpp"
+using namespace std;
+
+class cars {
+    public:
+      string name;
+      int hp;
+      int year;
+    void getCarInfo (string name, int hp, int year) {
+        cout<< "Name: " << name;
+        cout<< "Hp: " << hp;
+        cout<< "year: " << year;
+    }
+};
+
+int main(){
+    cars car;
+    car1.name("Mazda", "178", "2020")
+    ca
+}
